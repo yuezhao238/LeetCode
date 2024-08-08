058130575a06150e586d7889384b9c4f73bb9fd8
Heathcliff <2826190541@qq.com>
2024-08-08 08:23:19 +0800

    update

--- 28.cpp
+++ 28.cpp
@@ -6,14 +6,14 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         vector<int> next(haystack.size() + needle.size() + 1);
-        int j = -1;
-        next[0] = -1;
+        int j = 0;
+        next[0] = 0;
         string concat = needle + "#" + haystack;
         for (int i = 1; i < concat.size(); i++) {
-            while (j >= 0 && concat[i] != concat[j+1]) j = next[j];
-            if (concat[i] == concat[j+1]) j++;
+            while (j > 0 && concat[i] != concat[j]) j = next[j-1];
+            if (concat[i] == concat[j]) j++;
             next[i] = j;
-            if (next[i] == needle.size() - 1) return i - needle.size() * 2;
+            if (next[i] == needle.size()) return i - needle.size() * 2;
         }
         return -1;
     }
